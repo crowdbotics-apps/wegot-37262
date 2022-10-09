b68c141e4891b464cf70f1f1cb2f989e578adce7
Crowdbotics <team@crowdbotics.com>
2022-10-09 03:36:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wegot_37262/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wegot_37262;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wegot_37262";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wegot_37262",
+  url: "https://wegot_37262.botics.co",
+  api: "https://wegot_37262.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
